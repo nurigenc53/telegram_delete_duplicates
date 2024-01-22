eef61724edf1a045306e970b05c9937e9de58216
nurigenc53 <144450151+nurigenc53@users.noreply.github.com>
2024-01-23 00:21:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 import telepot
 
-bot_token = 'telegram bot token'
-channel_id = '@channel_name or -100id'
+bot_token = '6795654515:AAGzmRNOEdJk35iXOE0zS6Y72kqs-YXTt8Y'
+channel_id = '@-1002114969195'
 error_count = 0
 # A program that listens to new messages in a telegram channel
 # and deletes the message from the chat if it is the same as the previous message
